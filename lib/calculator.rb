7c914a1f0ebae894bf1028e01368697aa20d14c1
Caio <caio.felipemotta@hotmail.com>
2019-08-01 11:50:06 -0300

    22. Matchers de Erros
    raise_error

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
   def sum(n1, n2)
     (n1 + n2)
   end
+
+  def div(n1, n2)
+    n1/n2
+  end
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -12,7 +12,16 @@ describe Calculator do
     it 'with negative numbers' do
       result = subject.sum(-5, -7)
 
-      expect(result).to eq -12
+      expect(result).to eq(-12)
+    end
+  end
+
+  context '#div' do
+    it "divide by 0 - Precisa passar em bloco pra interpretar o erro" do
+      expect { subject.div(3, 0) }.to raise_error ZeroDivisionError # Passando o tipo do erro
+      expect { subject.div(3, 0) }.to raise_error 'divided by 0' # Passando a mensagem do erro
+      expect { subject.div(3, 0) }.to raise_error ZeroDivisionError, 'divided by 0' # Passando o tipo e a mensagem do erro
+      expect { subject.div(3, 0) }.to raise_error(/divided/) # Passando o regex da mensagem do erro
     end
   end
 end
