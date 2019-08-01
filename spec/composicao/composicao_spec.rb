7c5cdbab565ed7ba20bc4e33b86cc889060eb635
Caio <caio.felipemotta@hotmail.com>
2019-08-01 15:00:48 -0300

    27. Composição de Expectativas
    and / or

---
+++ spec/composicao/composicao_spec.rb
@@ -0,0 +1,8 @@
+describe 'Ruby on Rails' do
+  it { is_expected.to start_with('Ruby').and end_with("Rails") }
+  it { expect(frutas).to eq('banana').or eq('laranja').or eq('uva')}
+
+  def frutas
+    %w[banana laranja uva].sample
+  end
+end
