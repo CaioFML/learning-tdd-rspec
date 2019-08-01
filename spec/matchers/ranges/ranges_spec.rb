9c15fe878733f9990574abf53a9776ac80be499a
Caio <caio.felipemotta@hotmail.com>
2019-08-01 14:48:18 -0300

    26. One-liner Syntax (Subject)
    is_expected
    e o it { }

--- spec/matchers/ranges/ranges_spec.rb
+++ spec/matchers/ranges/ranges_spec.rb
@@ -1,7 +1,7 @@
 describe (1..5), "Ranges" do
-  it "#cover" do
-    expect(subject).to cover(2)
-    expect(subject).to cover(2, 5)
-    expect(subject).not_to cover(0, 6)
+  context "#cover" do
+    it { is_expected.to cover(2) }
+    it { expect(subject).to cover(2, 5) }
+    it { expect(subject).not_to cover(0, 6) }
   end
 end
