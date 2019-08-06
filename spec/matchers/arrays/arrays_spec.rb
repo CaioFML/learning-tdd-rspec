a875384632eb5cb9b767babff8c05540794ac31e
Caio <caio.felipemotta@hotmail.com>
2019-08-06 12:23:02 -0300

    41. Tag Filters
    Adicionar uma tag em um teste e colocar ~:tag no .rspec irá fazer com que pule aqueles testes, como o :slow

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]) do
+describe Array.new([1, 2, 3]), 'Array', type: 'collection' do
   it '#include' do
     expect(subject).to include 2
     expect(subject).to include 2, 1
@@ -8,7 +8,7 @@ describe Array.new([1,2,3]) do
 
   it { is_expected.to exclude 4 }
 
-  it '#match_array' do
+  it '#match_array', :slow do
     expect(subject).to match_array [1, 2, 3]
   end
 
