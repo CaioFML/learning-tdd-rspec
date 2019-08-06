1861166a14b6252b060a38b90623ff2874d95a71
Caio <caio.felipemotta@hotmail.com>
2019-08-05 21:35:14 -0300

    37. Negativando Matchers
    "RSpec::Matchers.define_negated_matcher :matcher_negado, :matcher"

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,9 +1,12 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3]) do
   it '#include' do
     expect(subject).to include 2
     expect(subject).to include 2, 1
-    expect(subject).to include 2, 1
   end
+
+  it { is_expected.to exclude 4 }
 
   it '#match_array' do
     expect(subject).to match_array [1, 2, 3]
