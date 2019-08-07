924cd66931f114d3b8203b84bbc86088ab44711b
Caio <caio.felipemotta@hotmail.com>
2019-08-06 21:02:29 -0300

    44. Method Stubs

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ class Student
   def has_finished?(course)
     ####### true || false #######
   end
+
+  def foo(arg)
+    ######
+  end
+
+  def bar
+    ####
+  end
 end
--- spec/stubs/stubs_spec.rb
+++ spec/stubs/stubs_spec.rb
@@ -3,9 +3,10 @@ require 'course'
 
 describe "Stub" do
   let(:student) { Student.new }
+  let(:other_student) { Student.new }
   let(:course) { Course.new }
 
-  it "Stubs has_finished?" do
+  it "#has_finished?" do
     allow(student)
       .to receive(:has_finished?)
       .with(an_instance_of(Course))
@@ -13,5 +14,31 @@ describe "Stub" do
 
     course_finished = student.has_finished?(course)
     expect(course_finished).to be_truthy
+  end
+
+  it "Argumentos dinamicos" do
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        "olá"
+      elsif arg == :hi
+        "Hi!!!"
+      end
+    end
+
+    expect(student.foo(:hello)).to eq 'olá'
+    expect(student.foo(:hi)).to eq 'Hi!!!'
+  end
+
+  it "Qualquer instancia de classe" do
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it "Erros" do
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect{ student.bar }.to raise_error RuntimeError
   end
 end
