5ed952d4ca88032d5ee967d927e3ba6a7296f27f
Caio <caio.felipemotta@hotmail.com>
2019-08-07 22:08:57 -0300

    47. Método "As Null Object"

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -8,4 +8,15 @@ describe "Test Double" do
     puts user.password
     puts user.age
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name: 'Caio', password: 'secret')
+    allow(user).to receive(:age).and_return('23')
+
+    expect(user.name).to eq 'Caio'
+    puts user.password
+    puts user.age
+    user.abc
+  end
 end
