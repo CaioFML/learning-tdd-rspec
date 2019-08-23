bed99ca2ee49b454c1bc449d26ac9f22324b568d
Caio <caio.felipemotta@hotmail.com>
2019-08-23 14:41:17 -0300

    51. Gem Faker

--- test_app/spec/factories/customer.rb
+++ test_app/spec/factories/customer.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    name { "Caio" }
-    email { "caio@motta.com" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
   end
 end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   let(:customer) { create(:customer) }
 
-  it 'create a customer' do
-    expect(customer.full_name).to eq 'Sr. Caio'
+  it '#full_name' do
+    expect(customer.full_name).to start_with 'Sr. '
   end
+
+  it { expect { create(:customer) }.to change { Customer.all.size}.by 1 }
 end
