502ae90e26307ad3ad21e2452dc4b137cf5d9ee1
Caio <caio.felipemotta@hotmail.com>
2019-08-23 16:26:05 -0300

    54. Attributes For

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -12,5 +12,11 @@ RSpec.describe Customer, type: :model do
     expect(customer.vip).to be_falsy
   end
 
+  it 'Usando attributes_for - Retorna um hash com os atributos' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with 'Sr. '
+  end
+
   it { expect { create(:user) }.to change { Customer.all.size }.by 1 }
 end
