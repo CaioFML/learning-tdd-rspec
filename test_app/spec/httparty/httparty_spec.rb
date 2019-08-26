336ea1aa23c15d5da38a40fbecac4a4c8594ec96
Caio <caio.felipemotta@hotmail.com>
2019-08-26 11:44:34 -0300

    66. Webmock

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe 'HTTParty' do
   it 'HTTParty get' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
+      .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
