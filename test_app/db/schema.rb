425a50a663819677d07ee9e9502d7ceb3c47891a
Caio <caio.felipemotta@hotmail.com>
2019-08-26 16:17:02 -0300

    74. Testando Models
    rails g rspec:model Model

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_125150) do
+ActiveRecord::Schema.define(version: 2019_08_26_191027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -34,5 +40,15 @@ ActiveRecord::Schema.define(version: 2019_08_26_125150) do
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "description"
+    t.decimal "price"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   add_foreign_key "orders", "customers"
+  add_foreign_key "products", "categories"
 end
