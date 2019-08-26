4756e9a2b196c1b764ced8c31e020337dbec68d7
Caio <caio.felipemotta@hotmail.com>
2019-08-26 15:43:42 -0300

    71. Modos de Gravação do VCR
    once
    new_episodes
    none
    all

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+describe 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
   it 'HTTParty get - webmock' do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
     #   .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
 
-    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
