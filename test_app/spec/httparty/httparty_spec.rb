108b3b61657c4d2825debf7f94e74157981c0672
Caio <caio.felipemotta@hotmail.com>
2019-08-26 11:53:29 -0300

    67. VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe 'HTTParty' do
-  it 'HTTParty get' do
-    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
-      .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
+  it 'HTTParty get - webmock' do
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
+    #   .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-    content_type = response.headers['content-type']
-    expect(content_type).to match(/application\/json/)
+    VCR.use_cassette("jsonplaceholder/posts") do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+      content_type = response.headers['content-type']
+      expect(content_type).to match(/application\/json/)
+    end
   end
 end
