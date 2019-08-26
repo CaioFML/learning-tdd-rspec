a4163a353a0d9068e53bdbf44f7a5b77bb574270
Caio <caio.felipemotta@hotmail.com>
2019-08-26 15:38:59 -0300

    70. VCR com URIs não determinísticas
    EXISTE VÁRIOS MATCHINGS E PODE SER ÚTIL, matching on body - vai verificar o content e não mais a url

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+describe 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
   it 'HTTParty get - webmock' do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
     #   .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
