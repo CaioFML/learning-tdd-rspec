266126f93afd19dbee8505103c63f98db50b9dc8
Caio <caio.felipemotta@hotmail.com>
2019-08-26 15:26:09 -0300

    68. VCR com metadados do RSpec

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
-describe 'HTTParty' do
+describe 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts' } do
   it 'HTTParty get - webmock' do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
     #   .to_return(status: 200, body: "", headers: { 'content-type': 'application/json; charset: utf-8' })
 
-    VCR.use_cassette("jsonplaceholder/posts") do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
-    end
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
 end
