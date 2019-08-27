3cb07fc89e8b419333ac27d8f962595bb553e0d5
Caio <caio.felipemotta@hotmail.com>
2019-08-27 12:20:36 -0300

    92. Testando APIs
    gem rspec-json_expectations

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
     it "works! (now write some real specs)" do
-      get customers_path
+      get customers_index_path
       expect(response).to have_http_status(200)
     end
   end
