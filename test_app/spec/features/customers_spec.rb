943459e20a7b2983405d0309a09cc9346c51a626
Caio <caio.felipemotta@hotmail.com>
2019-08-27 11:07:06 -0300

    85. Configurando o Screenshot (CHROMEDRIVER TAMBÉM)
    sudo apt-get install chromium-chromedriver

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "Customers", type: :feature do
+RSpec.feature "Customers", type: :feature, js: true do
   it 'visit index page' do
     visit customers_path
+    page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
   end
 end
