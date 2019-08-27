b9ea5e2fafe2feb4fd3116f387753f3da1ba957e
Caio <caio.felipemotta@hotmail.com>
2019-08-27 13:30:28 -0300

    95. POST - JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "Customers", type: :request do
 
     it "index - JSON 200 OK" do
       get '/customers.json'
-      expect(response).to have_http_status(200)
       expect(response.body).to include_json([
           id: /\d/,
           name: (be_kind_of String),
@@ -19,12 +18,27 @@ RSpec.describe "Customers", type: :request do
 
     it "show - JSON 200 OK" do
       get '/customers/37.json'
-      expect(response).to have_http_status(200)
       expect(response.body).to include_json(
           id: /\d/,
           name: (be_kind_of String),
           email: (be_kind_of String)
           )
+    end
+
+    it 'Create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT": "application/json" }
+
+      customers_params = attributes_for(:customer)
+
+      post "/customers.json", params: { customer: customers_params }, headers: headers
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email)
+        )
     end
   end
 end
