88d631a35d573c0e7b145351489222f972b8cf42
Caio <caio.felipemotta@hotmail.com>
2019-08-27 15:05:16 -0300

    103. Projetando a App

--- tdd_app/spec/features/welcome_spec.rb
+++ tdd_app/spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario "shows welcome message" do
-    visit('/')
+    visit root_path
     expect(page).to have_content 'Bem-Vindo'
+  end
+
+  scenario "verified link to create new clients" do
+    visit root_path
+    expect(find('ul li')).to have_link 'Cadastro de clientes'
   end
 end
