46bfc676a630d30b92f7896596f55736bb23b935
Caio <caio.felipemotta@hotmail.com>
2019-08-27 13:10:23 -0300

    94. RSpec Matchers para include_json

--- test_app/db/seeds.rb
+++ test_app/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Customer.create(name: Faker::Name.name, email: Faker::Internet.email, address: Faker::Address.street_address)
--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -11,17 +11,19 @@ RSpec.describe "Customers", type: :request do
       get '/customers.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json([
-          id:1,
-          name: "Teste",
-          email: "teste@teste"
-      ])
+          id: /\d/,
+          name: (be_kind_of String),
+          email: (be_kind_of String)
+        ])
     end
 
     it "show - JSON 200 OK" do
-      get '/customers/1.json'
+      get '/customers/37.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(
-          id:1,
+          id: /\d/,
+          name: (be_kind_of String),
+          email: (be_kind_of String)
           )
     end
   end
